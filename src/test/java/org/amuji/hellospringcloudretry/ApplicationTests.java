a689aeee02180e26b82111a959a8c3abca599f91
Roy Yang <aroundall@gmail.com>
2022-12-26 18:14:59 +0800

    blank project

---
+++ src/test/java/org/amuji/hellospringcloudretry/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amuji.hellospringcloudretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
